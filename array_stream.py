16665327cef37aaa05e3a03adf330bcb2686c269
Jared Ellison <jaredellison@gmail.com>
2019-06-15 18:08:19 -0400

    Add templates to script and get filename from input

--- array_stream.py
+++ array_stream.py
@@ -1,29 +1,63 @@
 import sys
 
-import normalize
+from sample_process import get_samples
+import templates
 
+# Get file name from command line input
 in_file = sys.argv[1]
+out_file = in_file.split(".")[0]
 
-def write_from_template(file_path):
-    template = open(file_path, "r")
-    for line in template:
-        sys.stdout.write(line)
-    template.close()
+# C Header templates
+params = {
+    "header_file_name": out_file,
+    "c_type": "uint8_t",
+    "c_type_max": 255
+}
 
-data = normalize.get_processed_data(in_file)
+top_template = f"""\
+/** @file {out_file}.h
+ *
+ * @brief Audio file formatted as c style array.
+ *
+ */
 
-write_from_template("./templates/header_template_pt1.h")
+#ifndef {params["header_file_name"].upper()}_H
+#define {params["header_file_name"].upper()}_H
+
+{params["c_type"]} sample = {{
+    """
+
+bottom_template = f"""
+}};
+
+#endif /* {params["header_file_name"].upper()}_H */
+
+/*** end of file ***/
+"""
+
+# Process in_file
+data = get_samples(in_file, params["c_type_max"])
+
+# Open output file
+out_file = open(out_file + '.h', 'w')
+
+# Write top of header file
+out_file.write(top_template)
 
 cols = 16
 col = 0
 
+# Write data
 for d in data[:-1]:
     col = col + 1
-    sys.stdout.write(f"{d:>3}, ")
+    out_file.write(f"{d}, ")
     if (col % cols == 0):
-        sys.stdout.write(f"\n    ")
+        out_file.write(f"\n    ")
 
 # No trailing comma for last line
-sys.stdout.write(f"{data[-1]}")
+out_file.write(f"{data[-1]}")
 
-write_from_template("./templates/header_template_pt2.h")
+# Write end of header file
+out_file.write(bottom_template)
+
+out_file.close()
